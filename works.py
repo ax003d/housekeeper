8d4ca2bcdc07710215eec64bad869692d3cbe050
ax003d <ax003d@gmail.com>
2014-11-08 16:21:15 +0800

    get public ip

--- works.py
+++ works.py
@@ -1,5 +1,26 @@
 import os
+import requests
 
 
 def disk_usage():
     return os.popen('df -h').read()
+
+def public_ip():
+    try:
+        resp = requests.get('http://httpbin.org/ip')
+        return resp.json()['origin']
+    except Exception, e:
+        pass
+    return "cannot get public ip."
+
+
+abilities = (
+    ('du', disk_usage, 'show disk usage'),
+    ('ip', public_ip, 'get house public ip'),
+)
+
+def do_work(msg):
+    for cmd, func, desc in abilities:
+        if msg == cmd:
+            return func()
+    return "Sorry, I don't know how to do it..."
